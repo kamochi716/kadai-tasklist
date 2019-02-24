0a7b56215a1dccdb32b42ed321c85990ce035206
satoshi.kawamoto <kamochi716@gmail.com>
2019-02-24 08:40:11 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,6 +9,7 @@
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -16,6 +17,7 @@
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
@@ -24,6 +26,5 @@
     @endif
     
       {!! link_to_route('tasks.create', '作成ページ', null, ['class' => 'btn btn-primary']) !!}
-
 
 @endsection
